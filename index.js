2f0d5efd4674d85b70632d581cfdd55aae8a6f77
Rashad Gilliam <akklaim83@gmail.com>
2022-01-31 19:11:05 -0800

    changes

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ let annualExpenses = 0
 window.prompt(weeklyExpensesQuestions[0])
 let answer = window.prompt(weeklyExpenseQuestions[0])
 console.log(answer)
+
+let totalAnnualExpenses = annualExpenses + (monthlyExpenses*12) + (weeklyExpenses*4*12)
+let state = { weeklyExpenses, monthlyExpenses, annualExpenses, totalAnnualExpenses }
+let elements = document.querySelectorAll('[data-bind]')
+elements.forEach( element => element.innerText = state[element.dataset.bind])
