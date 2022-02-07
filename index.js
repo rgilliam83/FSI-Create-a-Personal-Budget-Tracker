7047a188e9a487817ebde1292d2154c76b559459
Rashad Gilliam <rashadgilliam@Rashads-MacBook-Pro.local>
2022-02-07 10:20:08 -0800

    changes

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-let weeklyExpenses = 0
-let monthlyExpenses = 0
-let annualExpenses = 0
+let weeklyExpenses = 0;
+let monthlyExpenses = 0;
+let annualExpenses = 0;
 
+window.prompt(weeklyExpenseQuestions[0]);
 
-let weeklyExpenses = 0
-let monthlyExpenses = 0
-let annualExpenses = 0
+let weeklyExpenses = 0;
+let monthlyExpenses = 0;
+let annualExpenses = 0;
 
-window.prompt(weeklyExpensesQuestions[0])
+window.prompt(weeklyExpensesQuestions[0]);
 let answer = window.prompt(weeklyExpenseQuestions[0])
-console.log(answer)
+console.log(answer);
 
 let totalAnnualExpenses = annualExpenses + (monthlyExpenses*12) + (weeklyExpenses*4*12)
 let state = { weeklyExpenses, monthlyExpenses, annualExpenses, totalAnnualExpenses }
